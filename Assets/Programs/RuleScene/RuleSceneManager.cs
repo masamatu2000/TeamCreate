9ffa86d337c07d257aa4e4a85b0095af7ea4a8ba
Masaki Akimoto <250579@jc-21.jp>
2026-08-27 21:53:37 +0900

    モデルを試しに入れていたよ

--- Assets/Programs/RuleScene/RuleSceneManager.cs
+++ Assets/Programs/RuleScene/RuleSceneManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using UnityEngine.InputSystem;
 
 public class RuleSceneManager : MonoBehaviour
 {
@@ -9,16 +10,41 @@ public class RuleSceneManager : MonoBehaviour
 
     private void Start()
     {
+        // 動画が最後まで再生されたとき
         videoPlayer.loopPointReached += OnVideoFinished;
     }
 
+    private void Update()
+    {
+        // スペースキーを押したらPlaySceneへ移動
+        if (Keyboard.current != null &&
+            Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            GoToPlayScene();
+        }
+    }
+
+    /// <summary>
+    /// 動画が最後まで再生されたとき
+    /// </summary>
     private void OnVideoFinished(VideoPlayer player)
+    {
+        GoToPlayScene();
+    }
+
+    /// <summary>
+    /// PlaySceneへ移動
+    /// </summary>
+    private void GoToPlayScene()
     {
         SceneManager.LoadScene("PlayScene");
     }
 
     private void OnDestroy()
     {
-        videoPlayer.loopPointReached -= OnVideoFinished;
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached -= OnVideoFinished;
+        }
     }
 }
